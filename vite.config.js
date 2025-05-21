2ace40fe1c99ef3b66a0faf4d42a4162de436d70
Harshini-Gunasekar <harshinig203@gmail.com>
2025-05-21 23:39:35 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',
+//     port: process.env.PORT || 5173, // Let Render inject the port
+//   },
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,6 +15,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 5173, // Let Render inject the port
-  },
+    port: process.env.PORT || 5173,
+    allowedHosts: ['consultancy-frontend.onrender.com'] // 👈 Add this line
+  }
 });
